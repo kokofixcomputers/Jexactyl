838768ca5d72eb29a9f4a82ec4d5805f3e9c33b1
kokodev <github@kokofixcomputers.serv00.net>
2024-10-12 16:36:38 -0700

    Update ApiKeyRepositoryInterface.php

--- app/Contracts/Repository/ApiKeyRepositoryInterface.php
+++ app/Contracts/Repository/ApiKeyRepositoryInterface.php
@@ -13,14 +13,14 @@ interface ApiKeyRepositoryInterface extends RepositoryInterface
     public function getAccountKeys(User $user): Collection;
 
     /**
-     * Get all the application API keys that exist for a specific user.
+     * Get all the application API keys that exist.
      */
-    public function getApplicationKeys(User $user): Collection;
+    public function getApplicationKeys(): Collection;
 
     /**
-     * Delete an account API key from the panel for a specific user.
+     * Delete an account API key from the panel.
      */
-    public function deleteAccountKey(User $user, string $identifier): int;
+    public function deleteAccountKey(string $identifier): int;
 
     /**
      * Delete an application API key from the panel for a specific user.
